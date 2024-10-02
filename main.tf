02c49fa5188818d98f8c9176116b6156084b9f03
Kanav Gathe <80106966+SlayerK15@users.noreply.github.com>
2024-10-02 20:22:35 +0530

    Added Variables.tf

--- main.tf
+++ main.tf
@@ -3,34 +3,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-variable "aws_region" {
-  description = "AWS Region"
-  type        = string
-  default     = "us-east-1"
-}
-
-variable "ecs_cluster_name" {
-  description = "ECS Cluster Name"
-  type        = string
-  default     = "medusa-cluster"
-}
-
-variable "ecs_service_name" {
-  description = "ECS Service Name"
-  type        = string
-  default     = "medusa-service"
-}
-
-variable "image_uri" {
-  description = "The URI of the Docker image to deploy"
-  type        = string
-}
-
-variable "subnets" {
-  description = "List of subnets"
-  type        = list(string)
-}
-
 # VPC Configuration
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
